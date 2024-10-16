a1ccda10dd41d9646599bb7e3cc2cbd43bd7d1dc
akmilia <akmilia@yandex.ru>
2024-10-16 09:27:13 +0300

    Добавьте файлы проекта.

---
+++ Main.Designer.cs
@@ -0,0 +1,493 @@
+﻿namespace trpo_automat
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            panel1 = new Panel();
+            Мокка = new RadioButton();
+            Латте = new RadioButton();
+            Чай_зеленый = new RadioButton();
+            Чай_черный = new RadioButton();
+            Кипяток = new RadioButton();
+            Двойной_шоколад = new RadioButton();
+            Горячий_шоколад = new RadioButton();
+            Ирландский_кофе = new RadioButton();
+            Двойной_эспрессо = new RadioButton();
+            Эспрессо = new RadioButton();
+            Капучино = new RadioButton();
+            Американо = new RadioButton();
+            panel3 = new Panel();
+            Каптинка_напиток = new PictureBox();
+            panel2 = new Panel();
+            button1 = new Button();
+            panel5 = new Panel();
+            Картинка_сдача = new PictureBox();
+            Купюры = new ListBox();
+            Монеты = new ListBox();
+            label2 = new Label();
+            Плюс = new Button();
+            Минус = new Button();
+            panel4 = new Panel();
+            Сахар = new Label();
+            Деньги = new Label();
+            panel6 = new Panel();
+            label1 = new Label();
+            panel7 = new Panel();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Каптинка_напиток).BeginInit();
+            panel2.SuspendLayout();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)Картинка_сдача).BeginInit();
+            panel4.SuspendLayout();
+            panel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(192, 255, 255);
+            panel1.Controls.Add(Мокка);
+            panel1.Controls.Add(Латте);
+            panel1.Controls.Add(Чай_зеленый);
+            panel1.Controls.Add(Чай_черный);
+            panel1.Controls.Add(Кипяток);
+            panel1.Controls.Add(Двойной_шоколад);
+            panel1.Controls.Add(Горячий_шоколад);
+            panel1.Controls.Add(Ирландский_кофе);
+            panel1.Controls.Add(Двойной_эспрессо);
+            panel1.Controls.Add(Эспрессо);
+            panel1.Controls.Add(Капучино);
+            panel1.Controls.Add(Американо);
+            panel1.Controls.Add(panel3);
+            panel1.Location = new Point(21, 98);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(476, 494);
+            panel1.TabIndex = 0;
+            // 
+            // Мокка
+            // 
+            Мокка.AutoSize = true;
+            Мокка.Font = new Font("Mongolian Baiti", 12F);
+            Мокка.Location = new Point(17, 274);
+            Мокка.Name = "Мокка";
+            Мокка.Size = new Size(125, 25);
+            Мокка.TabIndex = 12;
+            Мокка.TabStop = true;
+            Мокка.Text = "Мокка 140р.";
+            Мокка.UseVisualStyleBackColor = true;
+            Мокка.CheckedChanged += Напиток;
+            // 
+            // Латте
+            // 
+            Латте.AutoSize = true;
+            Латте.Font = new Font("Mongolian Baiti", 12F);
+            Латте.Location = new Point(258, 274);
+            Латте.Name = "Латте";
+            Латте.Size = new Size(115, 25);
+            Латте.TabIndex = 11;
+            Латте.TabStop = true;
+            Латте.Text = "Латте 100р";
+            Латте.UseVisualStyleBackColor = true;
+            Латте.CheckedChanged += Напиток;
+            // 
+            // Чай_зеленый
+            // 
+            Чай_зеленый.AutoSize = true;
+            Чай_зеленый.Font = new Font("Mongolian Baiti", 12F);
+            Чай_зеленый.Location = new Point(258, 228);
+            Чай_зеленый.Name = "Чай_зеленый";
+            Чай_зеленый.Size = new Size(160, 25);
+            Чай_зеленый.TabIndex = 10;
+            Чай_зеленый.TabStop = true;
+            Чай_зеленый.Text = "Чай зеленый 45р.";
+            Чай_зеленый.UseVisualStyleBackColor = true;
+            Чай_зеленый.CheckedChanged += Напиток;
+            // 
+            // Чай_черный
+            // 
+            Чай_черный.AutoSize = true;
+            Чай_черный.Font = new Font("Mongolian Baiti", 12F);
+            Чай_черный.Location = new Point(17, 228);
+            Чай_черный.Name = "Чай_черный";
+            Чай_черный.Size = new Size(151, 25);
+            Чай_черный.TabIndex = 9;
+            Чай_черный.TabStop = true;
+            Чай_черный.Text = "Чай черный 40р.";
+            Чай_черный.UseVisualStyleBackColor = true;
+            Чай_черный.CheckedChanged += Напиток;
+            // 
+            // Кипяток
+            // 
+            Кипяток.AutoSize = true;
+            Кипяток.Font = new Font("Mongolian Baiti", 12F);
+            Кипяток.Location = new Point(258, 180);
+            Кипяток.Name = "Кипяток";
+            Кипяток.Size = new Size(127, 25);
+            Кипяток.TabIndex = 8;
+            Кипяток.TabStop = true;
+            Кипяток.Text = "Кипяток 30р.";
+            Кипяток.UseVisualStyleBackColor = true;
+            Кипяток.CheckedChanged += Напиток;
+            // 
+            // Двойной_шоколад
+            // 
+            Двойной_шоколад.AutoSize = true;
+            Двойной_шоколад.Font = new Font("Mongolian Baiti", 12F);
+            Двойной_шоколад.Location = new Point(17, 180);
+            Двойной_шоколад.Name = "Двойной_шоколад";
+            Двойной_шоколад.Size = new Size(199, 25);
+            Двойной_шоколад.TabIndex = 7;
+            Двойной_шоколад.TabStop = true;
+            Двойной_шоколад.Text = "Двойной шоколад 65р.";
+            Двойной_шоколад.UseVisualStyleBackColor = true;
+            Двойной_шоколад.CheckedChanged += Напиток;
+            // 
+            // Горячий_шоколад
+            // 
+            Горячий_шоколад.AutoSize = true;
+            Горячий_шоколад.Font = new Font("Mongolian Baiti", 12F);
+            Горячий_шоколад.Location = new Point(258, 132);
+            Горячий_шоколад.Name = "Горячий_шоколад";
+            Горячий_шоколад.Size = new Size(195, 25);
+            Горячий_шоколад.TabIndex = 6;
+            Горячий_шоколад.TabStop = true;
+            Горячий_шоколад.Text = "Горячий шоколад 60р.";
+            Горячий_шоколад.UseVisualStyleBackColor = true;
+            Горячий_шоколад.CheckedChanged += Напиток;
+            // 
+            // Ирландский_кофе
+            // 
+            Ирландский_кофе.AutoSize = true;
+            Ирландский_кофе.Font = new Font("Mongolian Baiti", 12F);
+            Ирландский_кофе.Location = new Point(17, 132);
+            Ирландский_кофе.Name = "Ирландский_кофе";
+            Ирландский_кофе.Size = new Size(207, 25);
+            Ирландский_кофе.TabIndex = 5;
+            Ирландский_кофе.TabStop = true;
+            Ирландский_кофе.Text = "Ирландский кофе 140р.";
+            Ирландский_кофе.UseVisualStyleBackColor = true;
+            Ирландский_кофе.CheckedChanged += Напиток;
+            // 
+            // Двойной_эспрессо
+            // 
+            Двойной_эспрессо.AutoSize = true;
+            Двойной_эспрессо.Font = new Font("Mongolian Baiti", 12F);
+            Двойной_эспрессо.Location = new Point(258, 88);
+            Двойной_эспрессо.Name = "Двойной_эспрессо";
+            Двойной_эспрессо.Size = new Size(201, 25);
+            Двойной_эспрессо.TabIndex = 4;
+            Двойной_эспрессо.TabStop = true;
+            Двойной_эспрессо.Text = "Двойной эспрессо 90р.";
+            Двойной_эспрессо.UseVisualStyleBackColor = true;
+            Двойной_эспрессо.CheckedChanged += Напиток;
+            // 
+            // Эспрессо
+            // 
+            Эспрессо.AutoSize = true;
+            Эспрессо.Font = new Font("Mongolian Baiti", 12F);
+            Эспрессо.Location = new Point(17, 88);
+            Эспрессо.Name = "Эспрессо";
+            Эспрессо.Size = new Size(137, 25);
+            Эспрессо.TabIndex = 3;
+            Эспрессо.TabStop = true;
+            Эспрессо.Text = "Эспрессо 85р.";
+            Эспрессо.UseVisualStyleBackColor = true;
+            Эспрессо.CheckedChanged += Напиток;
+            // 
+            // Капучино
+            // 
+            Капучино.AutoSize = true;
+            Капучино.Font = new Font("Mongolian Baiti", 12F);
+            Капучино.Location = new Point(258, 48);
+            Капучино.Name = "Капучино";
+            Капучино.Size = new Size(145, 25);
+            Капучино.TabIndex = 2;
+            Капучино.TabStop = true;
+            Капучино.Text = "Капучино 100р.";
+            Капучино.UseVisualStyleBackColor = true;
+            Капучино.CheckedChanged += Напиток;
+            // 
+            // Американо
+            // 
+            Американо.AutoSize = true;
+            Американо.Font = new Font("Mongolian Baiti", 12F);
+            Американо.Location = new Point(17, 48);
+            Американо.Name = "Американо";
+            Американо.Size = new Size(156, 25);
+            Американо.TabIndex = 1;
+            Американо.TabStop = true;
+            Американо.Text = "Американо 100р.";
+            Американо.UseVisualStyleBackColor = true;
+            Американо.CheckedChanged += Напиток;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ActiveCaptionText;
+            panel3.Controls.Add(Каптинка_напиток);
+            panel3.Location = new Point(157, 327);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(154, 164);
+            panel3.TabIndex = 0;
+            // 
+            // Каптинка_напиток
+            // 
+            Каптинка_напиток.BackColor = SystemColors.ActiveCaptionText;
+            Каптинка_напиток.Image = (Image)resources.GetObject("Каптинка_напиток.Image");
+            Каптинка_напиток.Location = new Point(13, 12);
+            Каптинка_напиток.Name = "Каптинка_напиток";
+            Каптинка_напиток.Size = new Size(127, 136);
+            Каптинка_напиток.SizeMode = PictureBoxSizeMode.StretchImage;
+            Каптинка_напиток.TabIndex = 0;
+            Каптинка_напиток.TabStop = false;
+            Каптинка_напиток.Click += pictureBox1_Click;
+            Каптинка_напиток.DoubleClick += Каптинка_напиток_DoubleClick;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(192, 255, 255);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(panel5);
+            panel2.Controls.Add(Купюры);
+            panel2.Controls.Add(Монеты);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(Плюс);
+            panel2.Controls.Add(Минус);
+            panel2.Controls.Add(panel4);
+            panel2.Font = new Font("Mongolian Baiti", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel2.Location = new Point(517, 14);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(238, 631);
+            panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 192, 192);
+            button1.Font = new Font("Sitka Subheading", 16.1999989F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(18, 436);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 86);
+            button1.TabIndex = 7;
+            button1.Text = "Приготовить";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = SystemColors.ActiveCaptionText;
+            panel5.Controls.Add(Картинка_сдача);
+            panel5.Location = new Point(47, 328);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(139, 86);
+            panel5.TabIndex = 6;
+            // 
+            // Картинка_сдача
+            // 
+            Картинка_сдача.BackColor = SystemColors.ActiveCaptionText;
+            Картинка_сдача.Image = Properties.Resources.pngwing_com__1_;
+            Картинка_сдача.Location = new Point(16, 16);
+            Картинка_сдача.Name = "Картинка_сдача";
+            Картинка_сдача.Size = new Size(105, 56);
+            Картинка_сдача.SizeMode = PictureBoxSizeMode.Zoom;
+            Картинка_сдача.TabIndex = 0;
+            Картинка_сдача.TabStop = false;
+            Картинка_сдача.Click += Картинка_сдача_Click;
+            // 
+            // Купюры
+            // 
+            Купюры.FormattingEnabled = true;
+            Купюры.ItemHeight = 18;
+            Купюры.Location = new Point(115, 216);
+            Купюры.Name = "Купюры";
+            Купюры.Size = new Size(96, 22);
+            Купюры.TabIndex = 5;
+            Купюры.SelectedIndexChanged += Купюры_SelectedIndexChanged;
+            // 
+            // Монеты
+            // 
+            Монеты.FormattingEnabled = true;
+            Монеты.ItemHeight = 18;
+            Монеты.Location = new Point(31, 216);
+            Монеты.Name = "Монеты";
+            Монеты.Size = new Size(62, 22);
+            Монеты.TabIndex = 4;
+            Монеты.SelectedIndexChanged += Монеты_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Mongolian Baiti", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(81, 268);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 24);
+            label2.TabIndex = 3;
+            label2.Text = "сахар";
+            // 
+            // Плюс
+            // 
+            Плюс.BackColor = Color.Teal;
+            Плюс.Font = new Font("Mongolian Baiti", 13.8F);
+            Плюс.Location = new Point(177, 266);
+            Плюс.Name = "Плюс";
+            Плюс.Size = new Size(34, 29);
+            Плюс.TabIndex = 2;
+            Плюс.Text = "+";
+            Плюс.UseVisualStyleBackColor = false;
+            Плюс.Click += Плюс_Click;
+            // 
+            // Минус
+            // 
+            Минус.BackColor = Color.Teal;
+            Минус.Font = new Font("Mongolian Baiti", 13.8F);
+            Минус.Location = new Point(18, 263);
+            Минус.Name = "Минус";
+            Минус.Size = new Size(34, 29);
+            Минус.TabIndex = 1;
+            Минус.Text = "-";
+            Минус.UseVisualStyleBackColor = false;
+            Минус.Click += button1_Click;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Navy;
+            panel4.Controls.Add(Сахар);
+            panel4.Controls.Add(Деньги);
+            panel4.Location = new Point(18, 84);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(197, 112);
+            panel4.TabIndex = 0;
+            // 
+            // Сахар
+            // 
+            Сахар.AutoSize = true;
+            Сахар.ForeColor = SystemColors.ButtonFace;
+            Сахар.Location = new Point(15, 46);
+            Сахар.Name = "Сахар";
+            Сахар.Size = new Size(0, 18);
+            Сахар.TabIndex = 1;
+            Сахар.Click += Сахар_Click;
+            // 
+            // Деньги
+            // 
+            Деньги.AutoSize = true;
+            Деньги.ForeColor = SystemColors.ButtonFace;
+            Деньги.Location = new Point(15, 13);
+            Деньги.Name = "Деньги";
+            Деньги.Size = new Size(0, 18);
+            Деньги.TabIndex = 0;
+            Деньги.Click += Деньги_Click;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.ButtonHighlight;
+            panel6.Controls.Add(label1);
+            panel6.Location = new Point(21, 16);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(476, 76);
+            panel6.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Sitka Subheading", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(115, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(248, 39);
+            label1.TabIndex = 0;
+            label1.Text = "Горячие напитки";
+            // 
+            // panel7
+            // 
+            panel7.BackColor = SystemColors.ButtonHighlight;
+            panel7.Location = new Point(21, 598);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(476, 47);
+            panel7.TabIndex = 3;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Teal;
+            ClientSize = new Size(785, 667);
+            Controls.Add(panel7);
+            Controls.Add(panel6);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Main";
+            Text = "Main";
+            Load += Main_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)Каптинка_напиток).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)Картинка_сдача).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel3;
+        private PictureBox Каптинка_напиток;
+        private Panel panel2;
+        private RadioButton Двойной_эспрессо;
+        private RadioButton Эспрессо;
+        private RadioButton Капучино;
+        private RadioButton Американо;
+        private RadioButton Мокка;
+        private RadioButton Латте;
+        private RadioButton Чай_зеленый;
+        private RadioButton Чай_черный;
+        private RadioButton Кипяток;
+        private RadioButton Двойной_шоколад;
+        private RadioButton Горячий_шоколад;
+        private RadioButton Ирландский_кофе;
+        private Button Минус;
+        private Panel panel4;
+        private Label Деньги;
+        private Label label2;
+        private Button Плюс;
+        private Label Сахар;
+        private Panel panel5;
+        private PictureBox Картинка_сдача;
+        private ListBox Купюры;
+        private ListBox Монеты;
+        private Panel panel6;
+        private Panel panel7;
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trpo_automat
+{
+    public partial class Main : Form
+    {
+        private string текущийНапиток1;
+        private int сахарКоличество = 0;
+        private int внесенныеДеньги = 0;
+        private Dictionary<string, int> напиткиЦены = new Dictionary<string, int>
+        {
+            {"Американо", 100},
+            {"Кипяток", 30},
+            {"Ирландский кофе", 140},
+            {"Двойной шоколад", 65},
+            {"Чай черный", 40},
+            {"Мокка", 140},
+            {"Капучино", 100},
+            {"Двойной эспрессо", 90},
+            {"Горячий шоколад", 60},
+            {"Чай зеленый", 45},
+            {"Латте", 100},
+            {"Эспрессо", 85}
+        };
+        public Main()
+        {
+            InitializeComponent();
+
+            Картинка_сдача.Visible = false;
+            Каптинка_напиток.Visible = false;
+
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Сахар.Text = "Сахар: " + сахарКоличество + " кубиков";
+
+            Монеты.Items.AddRange(new string[] { "10 р.", "5 р.", "1 р." });
+            Купюры.Items.AddRange(new string[] { "500 р.", "200 р.", "100 р.", "50 р.", "10 р." });
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Деньги_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Сахар_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Плюс_Click(object sender, EventArgs e)
+        {
+            if (сахарКоличество < 4)
+            {
+                сахарКоличество++;
+                ОбновитьСахар();
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (сахарКоличество > 0)
+            {
+                сахарКоличество--;
+                ОбновитьСахар();
+            }
+        }
+
+        private void ОбновитьСахар()
+        {
+            Сахар.Text = "Сахар: " + сахарКоличество + " кубиков";
+        }
+
+
+        private void Монеты_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            switch (Монеты.SelectedItem.ToString())
+            {
+                case "10 р.": внесенныеДеньги += 10; break;
+                case "5 р.": внесенныеДеньги += 5; break;
+                case "1 р.": внесенныеДеньги += 1; break;
+            }
+            ОбновитьДеньги();
+        }
+
+        private void Купюры_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (Купюры.SelectedItem.ToString())
+            {
+                case "500 р.": внесенныеДеньги += 500; break;
+                case "200 р.": внесенныеДеньги += 200; break;
+                case "100 р.": внесенныеДеньги += 100; break;
+                case "50 р.": внесенныеДеньги += 50; break;
+                case "10 р.": внесенныеДеньги += 10; break;
+            }
+
+            ОбновитьДеньги();
+
+        }
+
+        private void Картинка_сдача_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ОбновитьДеньги()
+        {
+            Деньги.Text = "Cумма: " + внесенныеДеньги + " р.";
+        }
+        private bool РассчитатьСдачу(string выбранныйНапиток)
+        {
+            if (!напиткиЦены.ContainsKey(выбранныйНапиток))
+            {
+                MessageBox.Show("Выбранный напиток не найден.");
+                return false;
+            }
+
+            int цена = напиткиЦены[выбранныйНапиток];
+
+            if (внесенныеДеньги >= цена)
+            {
+                int сдача = внесенныеДеньги - цена;
+                Деньги.Text = $"Сдача: {сдача} р.";
+                return true;
+            }
+
+            int нехватка = цена - внесенныеДеньги;
+            Деньги.Text = $"Недостаточно средств. Не хватает: {нехватка} р.";
+
+            return false;
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (РассчитатьСдачу(текущийНапиток1))
+            {
+                MessageBox.Show($"Для вас готовится {текущийНапиток1} ... ");
+                Картинка_сдача.Visible = true;
+                Каптинка_напиток.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show($"У вас недостаточно средств на {текущийНапиток1}");
+            }
+        }
+
+        private void Каптинка_напиток_DoubleClick(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Вы забрали напиток!");
+            сахарКоличество = 0;
+            внесенныеДеньги = 0;
+            Деньги.Text = "Cумма: " + внесенныеДеньги + " р.";
+            Сахар.Text = "Сахар: " + сахарКоличество + " кубиков";
+
+            Картинка_сдача.Visible = false;
+            Каптинка_напиток.Visible = false;
+
+
+        }
+
+        private void Напиток(object sender, EventArgs e)
+        {
+
+            RadioButton selectedRadioButton = sender as RadioButton;
+            if (selectedRadioButton != null && selectedRadioButton.Checked)
+            {
+               
+                string[] parts = selectedRadioButton.Text.Split(' ');
+                if (parts.Length > 1)
+                {
+                    текущийНапиток1 = string.Join(" ", parts, 0, parts.Length - 1); // Join all but the last part
+                }
+                else
+                {
+                    текущийНапиток1 = parts[0]; // Fallback in case there's no space
+                }
+
+                РассчитатьСдачу(текущийНапиток1); // Call to calculate change
+            }
+        }
+    }
+}
